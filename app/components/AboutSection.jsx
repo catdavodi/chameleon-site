9d99a3081e1c25435220c27fa6b1fd655eb67e46
catdavodi <125699432+catdavodi@users.noreply.github.com>
2023-11-04 01:57:12 -0700

    fix apos

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -42,20 +42,20 @@ const AboutSection = () => {
     return (
         <section id="about" className="text-white">
             <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 mt-48 mb-32">
-                <Image src="/images/pic.png" width={700} height={700} mr-10 />
+                <Image src="/images/pic.png" alt="pic" width={700} height={700} mr-10 />
                 <div>
                     <h2 className="text-4xl font-bold text-white mb-4">About Us</h2>
                     <p className="text-base lg:text-lg text-[#ADB7BE]">
                     Recruiters consistently advise tailoring your resume for each role. However, achieving 
                     this for every job is time-wasting, inefficient, and the biggest drop off for early 
-                    career applicants. With Chameleon, it's easy: users drop in their resume, upload all 
+                    career applicants. With Chameleon, it&apos;s easy: users drop in their resume, upload all 
                     the jobs they&apos;re applying to, and a user-friendly AI Resume Editor and organization tool. 
                     It helps tailor content with industry-specific suggestions and creates unique versions 
                     for different job positions. Chameleon also organizes and structures your numerous resumes 
                     by industry and company so you can finally keep track, find, and sync edits across any and 
                     all of your resumes. Bringing everything together, Chameleon&apos;s chrome extension allows 
                     applicants to instantly customize their resume for the role on-demand and autofill the rest of 
-                    the application with their information to streamline the process. We're confident that 
+                    the application with their information to streamline the process. We&apos;re confident that 
                     Chameleon will transform the way early-career applicants approach their resumes, 
                     streamlining management, editing, tailoring, and submission, ensuring users always present 
                     their best colors in each application.
